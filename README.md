dd63952b151cdfb1f7a28759b339894aefae982c
avi tshuva <dharmax@gmail.com>
2020-10-28 17:12:07 +0200

    added: docs added: typing added: README.md added: functions populateFields, populateField

--- README.md
+++ README.md
@@ -1,3 +1,83 @@
+# Field-Assist
+## General
 This little, light library makes working with form and input field very easy, normalized and fun.
 
-It's main function is collectValues( baseElement)
+It's main function is collectValues( baseElement) that basically does the following:
+1. Recursively collect all the values from any input fields under the baseElement that have a "ref" attribute (with a logical name as a value).
+1. Places them in a simple map, where the ref value is the entry's key, where its  is the **normalized** from the input field.
+1. It validates the values and create an error object (the "_error") in the returned map
+      
+## Normalization
+The normalization means that checkable items (radio buttons groups, multi-selects, checkboxes) appear in the map as 
+you would expect them to appear: as booleans, a single value or an array of values, so it would be trivial to work with.
+
+## Validation
+The validation supports the standard modern HTML validation, of course, plus the ability to easily add a custom javascript
+validator, using an attribute "validator".
+
+If a value failed validation, the map entry for it would be the symbol Invalid and its erroneous value would still be easily
+accessible via the map under the "_errors" entry.
+
+### custom validator
+
+The custom validator should be defined in the "validator" attribute and should be a JS function that **returns a function** 
+with the following signature: f( value, context): boolean. Very simple. The context is the option context object that
+you may pass to the collectValues method.  
+
+## Update-as-you-type
+
+The function getFieldAndValue(event) is useful for PWA-style update-as-you-type style. See example. Internally
+it uses the same logic, but it finds the input element that was just changed, so you can process further only it, instead 
+of all the values. Pretty useful.  
+
+# example      
+
+*This is a Riot JS file snippet, but it is irrelevant for understanding the usage of the library  
+```
+<form oninput="{update}">
+    <label>Display Name:
+        <input ref="name" type="text" value="{state.profile.name}">
+    </label>
+    <div class="bio">
+        <label>About myself</label><br>
+         <textarea ref="generalBio" cols="30" rows="10">{state.profile.generalBio}</textarea>
+    </div>
+    <div class="main-language">
+    <label>Main Language
+        <select autocomplete="on" ref="preferredLanguage" value="{state.profile.preferredLanguage}">
+                <option each="{l in state.languages}" selected="{state.profile.preferredLanguage===l}" value="{l}">
+                    {l}
+                </option>
+        </select>
+    </label>
+</form>
+<script>
+    import {userStore} from "../../viewmodel/user-store";
+    input {getFieldAndValue, collectValues} from 'field-assist' 
+
+    export default {
+        
+        async onMounted() {
+            const profile = await userStore.getMyProfile()
+            const languages = ['en', 'he']
+            this.update({profile, languages}      
+        }
+        update(event) {
+
+                // there are two options here, you need to use the one that suit you most
+
+                // this option updates all at once:
+
+                userStore.updateMe( collectValues(this.$))
+
+                // this option updates a single field:
+
+                let {field, value} = getFieldAndValue(event);
+                // noinspection JSIgnoredPromiseFromCall
+                userStore.updateMe({[field]: value})
+        }
+
+    }
+
+</script>
+```
--- src/field-assist.ts
+++ src/field-assist.ts
@@ -1,33 +1,4 @@
 /**
- *
- * @param node start search node
- * @param keyFieldName optional alternative reference field name
- * @return array of elements that have the "ref" attribute
- */
-export function refs(node: Element, keyFieldName = 'ref'): HTMLInputElement[] {
-    const nodes = node.querySelectorAll(`[${keyFieldName}]`)
-    // @ts-ignore
-    return Array.of(...nodes) as HTMLInputElement[]
-}
-
-/**
- * @return elements map by the ref name
- * @param node
- * @param keyFieldName optional alternative reference field name
- */
-export function refNodes(node: Element, keyFieldName = 'ref'): { [ref: string]: HTMLInputElement } {
-
-    const map: { [ref: string]: HTMLInputElement } = {}
-    const elements = refs(node, keyFieldName)
-    elements.reduce((a, c: HTMLElement) => {
-        // @ts-ignore
-        a[c.getAttribute('ref')] = c
-        return a
-    }, map)
-    return map
-}
-
-/**
  * Looks for elements with the "ref" (or whatever other  attribute then returns a map with the values
  * inside those elements, with the string in the "ref" as the name.
  * It also mark invalid values with Invalid symbol.
@@ -51,7 +22,7 @@ export function collectValues(node: Element, context?: any, keyFieldName = 'ref'
                 e.nodeValue || e.getAttribute('value') || e['value'] || e.textContent
 
         const customValidator = e.getAttribute('validator')
-        let isValid = true
+        let isValid
         if (customValidator) {
             const validationFunction = eval(customValidator.toString());
             isValid = validationFunction(value, context)
@@ -79,5 +50,93 @@ export function collectValues(node: Element, context?: any, keyFieldName = 'ref'
         return values
     }
 }
+
+/**
+ *
+ * @param node start search node
+ * @param keyFieldName optional alternative reference field name
+ * @return array of elements that have the "ref" attribute
+ */
+export function refs(node: Element, keyFieldName = 'ref'): HTMLInputElement[] {
+    const nodes = node.querySelectorAll(`[${keyFieldName}]`)
+    // @ts-ignore
+    return Array.of(...nodes) as HTMLInputElement[]
+}
+
+/**
+ * @return elements map by the ref name
+ * @param node
+ * @param keyFieldName optional alternative reference field name
+ */
+export function refNodes(node: Element, keyFieldName = 'ref'): { [ref: string]: HTMLInputElement } {
+
+    const map: { [ref: string]: HTMLInputElement } = {}
+    const elements = refs(node, keyFieldName)
+    elements.reduce((a, c: HTMLElement) => {
+        // @ts-ignore
+        a[c.getAttribute(keyFieldName)] = c
+        return a
+    }, map)
+    return map
+}
+
+/**
+ * Utility method that let you get the field name and value of the input field that is associated with an event
+ * @param event the event
+ * @param context optional context for the collectValue inner call
+ * @param keyFieldName optional alternative to the "ref" attribute name
+ */
+export function getFieldAndValue(event: Event, context?: any, keyFieldName = 'ref'): { field: string, value: any } {
+    let target = event.target as HTMLElement;
+    if (!target.getAttribute(keyFieldName))
+        target = target.parentNode as HTMLElement
+    const fieldName = target.getAttribute(keyFieldName) as string
+    const values = collectValues(target, context, keyFieldName)
+    return {field: fieldName, value: values[fieldName]};
+}
+
+/**
+ * Populate a single field in a normalized way
+ * @param node the field's node
+ * @param value the value
+ */
+export function populateField(node: HTMLInputElement, value: string | number | boolean | string[]) {
+
+    if (!(node.type in ['checkbox', 'select', 'option', 'radio', 'textarea', 'select-one', 'select-multi'])) {
+        // @ts-ignore
+        node.value = value.toString()
+        return
+    }
+
+    if ('checked' in node) {
+        // @ts-ignore
+        node.checked = node.value in value
+        return;
+    }
+    if ('selected' in node) {
+        // @ts-ignore
+        node.selected = node.value in value
+        return;
+    }
+
+    if ('textContent' in node) {
+        // @ts-ignore
+        node.textContent = value.toString()
+        return
+    }
+}
+
+/**
+ * Populate a whole form from a map of field names and values
+ * @param baseNode the base of the form (doesn't have to be a Form element)
+ * @param values the values
+ */
+export function populateFields(baseNode: HTMLElement, values: { [ref: string]: string | string[] | boolean | number }) {
+    const fieldMap = refNodes(baseNode)
+    for (let [ref, node] of Object.entries(fieldMap))
+        populateField(node, values[ref])
+
+}
+
 
 export const Invalid = Symbol('Invalid')
