f21cf9fc6902493427e8c9acc39bf7d4ae873aaa
dharmax <dharmax@gmail.com>
2022-03-24 14:41:25 +0200

    added: some documentation

--- lib/auto-form.d.ts
+++ lib/auto-form.d.ts
@@ -1,3 +1,9 @@
+/**
+ * This class generate forms out of raw data and optional meta-data.
+ * It supports automatic translation (of labels and display values).
+ * It generates raw DOM elements without styling, so it is your responsibility to style it (which is very easy).
+ *
+ */
 export declare class AutoForm {
     metaData: FormMetaData;
     /**
@@ -5,11 +11,28 @@ export declare class AutoForm {
      * @param s
      */
     textTranslator: (s: string) => string;
+    /**
+     * The generated form.
+     */
     readonly form: HTMLFormElement;
+
+    /**
+     * You can provide the metadata right in the constructor or anytime before rendering
+     * @param metaData the metadata
+     * @see FormMetaData
+     */
     constructor(metaData?: FormMetaData);
+
+    /**
+     * This method does the DOM element creation
+     * @param data the data by which to "guess" the form
+     * @param root optionally, you can tell it to render right into that element
+     * @return the generated form
+     */
     render(data: any, root?: HTMLElement): HTMLFormElement;
+
     private renderFields;
-    getFieldMetaData(fieldName: string, value: any, _parents: string[]): FieldMetaData;
+    private getFieldMetaData;
     private renderField;
     private guessType;
 }
